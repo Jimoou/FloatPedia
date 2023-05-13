c1066275191d7c0d616a34b53ed96c1c9ffacdd2
Jiwoon <kfromh0136@Naver.com>
2023-05-13 22:34:29 +0900

    [Chore] : install-SpringCloud-MS-doubleOperation-service
    #dependency
    * spring-boot-starter-web
    * springdoc-openapi-ui:1.6.15
    * validation
    * netflix-eureka-client
    * actuator
    * spring-cloud-starter-config
    * spring-cloud-starter-bus-amqp
    * spring-cloud-starter-circuitbreaker-resilience4j
    * aop
    * lombok

---
+++ doubleOperation-service/src/main/java/github/jimoou/doublesimulator/DoubleSimulatorApplication.java
@@ -0,0 +1,13 @@
+package github.jimoou.doublesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleSimulatorApplication.class, args);
+	}
+
+}
